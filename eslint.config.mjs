19bec30b762efa9b06f9219733502a1f4a8cebd4
chertik77 <denysbabych.dev@gmail.com>
2024-08-28 19:45:02 +0100

    chore: add ts-eslint parser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import { fixupPluginRules } from '@eslint/compat'
 import js from '@eslint/js'
 import next from '@next/eslint-plugin-next'
+import tsParser from '@typescript-eslint/parser'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 import tailwind from 'eslint-plugin-tailwindcss'
@@ -12,23 +12,24 @@ import tailwind from 'eslint-plugin-tailwindcss'
 export default [
   { ignores: ['.next/**'] },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { parser: tsParser } },
   js.configs.recommended,
   react.configs.flat.recommended,
   ...tseslint.configs.recommended,
   ...tailwind.configs['flat/recommended'],
   {
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': fixupPluginRules(reactHooks),
       '@next/next': fixupPluginRules(next)
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...next.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       ...next.configs['core-web-vitals'].rules,
       'react/react-in-jsx-scope': 'off',
-      'react/display-name': 'off',
-      '@typescript-eslint/no-require-imports': 'off'
+      'react/display-name': 'off'
     }
   }
 ]
